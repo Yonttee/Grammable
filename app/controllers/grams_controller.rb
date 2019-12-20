b6fb64010aabbc7afc59c6e52f56299b725ffb68
Carriane Linzer <carrianelinzer@icloud.com>
2019-12-20 03:46:43 +0000

    debugging

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -15,6 +15,11 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    render plain: 'Not Found :(', status: :not_found
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,18 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#show action"
+
+  describe "grams#edit action" do 
+    it "should successfully show the edit form if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end 
+
+    it "should return a 404 error message if the gram is not found" do
+      get :edit, params: { id: 'SWAG' }
+      expect(response).to have_http_status(:not_found)
+    end 
+  end
+
+  describe "grams#show action" do 
     it "should successfully show the page if the gram is found" do 
       gram = FactoryBot.create(:gram)
       get :show, params: { id: gram.id }
       expect(response).to have_http_status(:success)
     end
+  
 
     it "should return a 404 error if the gram is not found" do 
       get :show, params: { id: 'TACOCAT'}
       expect(response).to have_http_status(:not_found)
     end 
-  
+  end
 
   describe "grams#index action" do 
     it "should successfully show the page" do
@@ -44,6 +59,7 @@ RSpec.describe GramsController, type: :controller do
       post :create, params: {gram: { message: "Hello"} }
       expect(response).to redirect_to new_user_session_path
     end
+
     it "should successfully create a new gram in our database" do
        user = FactoryBot.create(:user)
 
